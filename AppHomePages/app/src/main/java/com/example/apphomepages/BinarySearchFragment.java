10adb104ed81ce4d1a7f7453cc378ead156ac883
Sarah Depew <sdepew@brynmawr.edu>
2019-01-31 21:54:08 -0500

    Adding Binary Search Visualization

--- AppHomePages/app/src/main/java/com/example/apphomepages/BinarySearchFragment.java
+++ AppHomePages/app/src/main/java/com/example/apphomepages/BinarySearchFragment.java
@@ -1,12 +1,24 @@
 package com.example.apphomepages;
 
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 
 /**
@@ -17,7 +29,7 @@ import android.view.ViewGroup;
  * Use the {@link BinarySearchFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BinarySearchFragment extends Fragment {
+public class BinarySearchFragment extends Fragment implements SpinnerAdapter {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -27,7 +39,13 @@ public class BinarySearchFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private int currentCount = 0;
+    private int bound = 10; //the max number of elements in the array
+    private int[] numbers = null;
+    private int soughtAfter = -1;
+    private Random r = new Random();
+    private LinearSearchDrawable[] stopMotionAnimation = null;
+    private OnFragmentInteractionListener mListener = null;
 
     public BinarySearchFragment() {
         // Required empty public constructor
@@ -63,8 +81,137 @@ public class BinarySearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_binary_search, container, false);
+        final View viewGlobal = inflater.inflate(R.layout.fragment_linear_search, container, false);
+
+        //Set up the buttons and clickable elements on the fragment
+        Button nextFrameButton = viewGlobal.findViewById(R.id.frameButton);
+        Button generateButton = viewGlobal.findViewById(R.id.generateButton);
+        final Spinner spinner = viewGlobal.findViewById(R.id.spinner);
+
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentCount = 0; //make sure we are starting at 0 in the array
+
+                int numElements = r.nextInt(bound) + 1; //want a value between 1 and 10, so 1-10 elements in the array
+                numbers = new int[numElements]; //random numbers
+
+                for (int j = 0; j < numElements; j++) {
+                    int randomInt = r.nextInt(bound * 10);
+                    if (r.nextBoolean()) {
+                        numbers[j] = (-randomInt);
+                    } else {
+                        numbers[j] = randomInt;
+                    }
+                }
+
+                Arrays.sort(numbers); //binary search requires a sorted array
+
+                //populate the spinner used https://stackoverflow.com/questions/20244419/android-spinner-populating-using-arrayliststring as a resource as well as https://stackoverflow.com/questions/10582283/how-to-populate-spinner-from-a-array-string
+                ArrayList<String> numbersStrings = new ArrayList<>();
+
+                for (int k = 0; k < numElements; k++) {
+                    numbersStrings.add(Integer.toString(numbers[k]));
+                }
+
+                ArrayAdapter adapter = new ArrayAdapter(viewGlobal.getContext(), android.R.layout.simple_spinner_item, numbersStrings);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+
+                boolean targetFound = r.nextBoolean();
+                soughtAfter = targetFound ? numbers[r.nextInt(numbers.length)] : -1;
+                ArrayList<Integer> squaresToHighlight = SearchingAlgorithms.binarySearchWithLocations(numbers, soughtAfter);
+                int locationInArray = squaresToHighlight.get(squaresToHighlight.size() - 1); //the last location is the place that we are trying to find
+
+                if (locationInArray != -1) {
+                    spinner.setSelection(locationInArray); //choose the element in the array to select automatically from the dropdown menu (a.k.a. spinner menu)
+                } else {
+                    //TODO: setting when not found!
+                }
+
+                int square = -1;
+
+                stopMotionAnimation = new LinearSearchDrawable[squaresToHighlight.size() + 1];
+
+                //TODO: get this info from the user and parse into array of "animation frames"
+                Color main = new Color(255, 0, 255);
+                Color secondary = new Color(255, 255, 0);
+                Color found = new Color(255, 0, 0);
+
+                //setup main frame
+                stopMotionAnimation[0] = new LinearSearchDrawable(main, secondary, found, square, false, numbers);
+                square++;
+
+                for (int i = 1; i < stopMotionAnimation.length; i++) {
+                    stopMotionAnimation[i] = locationInArray == (i - 1) ? new LinearSearchDrawable(main, secondary, found, squaresToHighlight.get(i - 1), true, numbers) : new LinearSearchDrawable(main, secondary, found, squaresToHighlight.get(i - 1), false, numbers);
+                    square++;
+                }
+
+                ImageView image = viewGlobal.findViewById(R.id.imageView);
+                image.setImageDrawable(stopMotionAnimation[currentCount]);
+                currentCount = 1;
+            }
+        });
+
+        /*
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                soughtAfter = Integer.valueOf((String) spinner.getSelectedItem());
+                currentCount = 0;
+                int locationInArray = SearchingAlgorithms.linearSearch(numbers, soughtAfter);
+                int square = -1;
+
+                //depending if the element is found or not...
+                if (locationInArray == -1) {
+                    stopMotionAnimation = new LinearSearchDrawable[numbers.length + 1];
+                } else {
+                    stopMotionAnimation = new LinearSearchDrawable[locationInArray + 1 + 1];
+                }
+
+                Color main = new Color(255, 0, 255);
+                Color secondary = new Color(255, 255, 0);
+                Color found = new Color(255, 0, 0);
+
+                //setup main frame
+                stopMotionAnimation[0] = new LinearSearchDrawable(main, secondary, found, square, false, numbers);
+                square++;
+
+                for (int i = 1; i < stopMotionAnimation.length; i++) {
+                    stopMotionAnimation[i] = locationInArray == (i - 1) ? new LinearSearchDrawable(main, secondary, found, square, true, numbers) : new LinearSearchDrawable(main, secondary, found, square, false, numbers);
+                    square++;
+                }
+
+                ImageView image = viewGlobal.findViewById(R.id.imageView);
+                image.setImageDrawable(stopMotionAnimation[currentCount]);
+                currentCount = 1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //do nothing
+            }
+        });
+        */
+
+        nextFrameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (stopMotionAnimation != null) {
+                    if (currentCount >= stopMotionAnimation.length) {
+                        currentCount = 0;
+                    }
+
+                    ImageView image = viewGlobal.findViewById(R.id.imageView);
+                    image.setImageDrawable(stopMotionAnimation[currentCount]);
+                    currentCount++;
+                }
+            }
+        });
+
+        return viewGlobal;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -89,6 +236,61 @@ public class BinarySearchFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
 
     /**
--- AppHomePages/app/src/main/java/com/example/apphomepages/SearchingAlgorithms.java
+++ AppHomePages/app/src/main/java/com/example/apphomepages/SearchingAlgorithms.java
@@ -1,5 +1,7 @@
 package com.example.apphomepages;
 
+import java.util.ArrayList;
+
 public class SearchingAlgorithms {
 
     public SearchingAlgorithms() {
@@ -7,12 +9,41 @@ public class SearchingAlgorithms {
     }
 
     public static int linearSearch(int[] numbers, int target) {
-        for(int i=0; i<numbers.length; i++) {
-            if(numbers[i] == target) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target) {
                 return i;
             }
         }
 
         return -1;
+    }
+
+    //used the algorithm found at this website for an iterative version of binary search https://medium.com/@gustavo.ponce.ch/binary-search-with-java-recursive-iterative-java-collections-5a12a814a27c
+    public static ArrayList<Integer> binarySearchWithLocations(int[] array, int search) {
+        ArrayList<Integer> squaresToHighlight = new ArrayList<>();
+
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start <= end) {
+
+            int middle = (start + end) / 2;
+            squaresToHighlight.add(middle);
+
+            if (search < array[middle]) {
+                end = middle - 1;
+            }
+
+            if (search > array[middle]) {
+                start = middle + 1;
+            }
+
+            if (search == array[middle]) {
+                squaresToHighlight.add(middle);
+                return squaresToHighlight;
+            }
+        }
+
+        return squaresToHighlight;
     }
 }
