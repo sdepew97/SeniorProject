8edc4c4550b827836b77d7baac339fa47bf18858
Sarah Depew <sdepew@brynmawr.edu>
2019-02-14 23:27:25 -0500

    Adding more front end graphs and proof links to buttons

--- AppHomePages/app/src/main/java/com/example/apphomepages/Activities/TopologicalOrderingActivity.java
+++ AppHomePages/app/src/main/java/com/example/apphomepages/Activities/TopologicalOrderingActivity.java
@@ -1,8 +1,12 @@
 package com.example.apphomepages.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.apphomepages.R;
 
@@ -17,6 +21,59 @@ public class TopologicalOrderingActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Set up the buttons and clickable elements on the fragment
+        Button generateButton = findViewById(R.id.generateButton);
+        Button startButton = findViewById(R.id.startButton);
+        Button stopButton = findViewById(R.id.stopButton);
+        Button rewindButton = findViewById(R.id.rewindButton);
+        Button proofButton = findViewById(R.id.proofButton);
+
+        generateButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        rewindButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+
+        proofButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Open a webpage! (resource used to figure out code at https://stackoverflow.com/questions/2201917/how-can-i-open-a-url-in-androids-web-browser-from-my-application)
+                Uri uri = Uri.parse("http://www.columbia.edu/~cs2035/courses/csor4231.F15/ts.pdf"); //TODO: find proof that doesn't require downloading anything!
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            }
+        });
     }
 
 }
